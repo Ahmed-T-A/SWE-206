1f22994dcf5ad027112e62df4149e8399d6e1258
Ahmed Talal <hamad5232@yahoo.ca>
2023-01-23 20:27:24 +0300

    final version of lab1

--- Labs/Lab1/Lab1/src/App.java
+++ Labs/Lab1/Lab1/src/App.java
@@ -1,19 +1,42 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        ArrayList<String> names = new ArrayList<>();
-        File myFile = new File("U:\\Term222{ICS202-SWE206-ISE291-MATH208-GS332}\\SWE206\\Labs\\Lab1\\List.txt");
-        Scanner input = new Scanner(myFile);
-
-        while(input.hasNext()){
-            names.add(input.next());
+    public static void main(String[] args){
+        try{
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the path of the list file: "); // Enter the path.
+            String fileName = input.next();
+            File students = new File(fileName); // "U:\\Term222{ICS202-SWE206-ISE291-MATH208-GS332}\\SWE206\\SWE-206\\Labs\\Lab1\\List.txt"
+            Scanner reader = new Scanner(students);
+            ArrayList studenList = new ArrayList<>();
+            while(reader.hasNext()){
+                String student = reader.next();
+                studenList.add(student);
+            }
+            System.out.print("Enter the way of distributing students:\n  1) By a fixed number of groups.\n  2) By a fixed maximum students.\nChoice: ");
+            int way = input.nextInt();
+            switch (way) {
+                case 1 :
+                    System.out.print("Enter the number of groups: ");
+                    int groupsNumber = input.nextInt();
+                    System.out.println(fixedNumberOfGroups(studenList, groupsNumber));
+                    break;
+                case 2 :
+                    System.out.print("Enter the maximum number of students: ");
+                    int maxSize = input.nextInt();
+                    System.out.println(fixedSizeGroups(studenList, maxSize));
+                    break;
+            }
         }
-        System.out.println(fixedSizeGroups(names, 5));
-        System.out.println(fixedNumberOfGroups(names, 3));
+        catch(FileNotFoundException ex){
+            System.out.println(ex.getMessage());
+        } 
     }
+
+    // Creating groups with a fixed number of students.
     public static ArrayList<ArrayList<String>> fixedSizeGroups( ArrayList<String> names, int size){
         int listSize = names.size();
         ArrayList<ArrayList<String>> groupsArray = new ArrayList<ArrayList<String>>();
@@ -32,6 +55,8 @@ public class App {
             }   
         return groupsArray;
     }
+
+    // Creating fixed number of groups.
     public static ArrayList<ArrayList<String>> fixedNumberOfGroups( ArrayList<String> names, int numOfGroups){
         int listSize = names.size();
         int numberOfStudentInEachGroup = 0;
